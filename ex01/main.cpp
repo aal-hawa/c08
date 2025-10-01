dd89e40661303dac967194669ffeb613404d6041
Anas Al Hawamda <aal-hawa@lab1r3s10.42abudhabi.ae>
2025-10-01 18:17:22 +0400

    edit

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,25 +6,25 @@
 int main()
 {
 	try {
-		Span sp = Span(5);
-		sp.addNumber(6);
-		sp.addNumber(3);
-		sp.addNumber(17);
-		sp.addNumber(9);
-		sp.addNumber(11);
-		std::cout << sp.shortestSpan() << std::endl;
-		std::cout << sp.longestSpan() << std::endl;
-	
-			// // Big test with 10,000 numbers
-			// Span bigSpan(10000);
-			// std::srand(time(NULL));
+			Span sp = Span(5);
+			sp.addNumber(6);
+			sp.addNumber(3);
+			sp.addNumber(17);
+			sp.addNumber(9);
+			sp.addNumber(11);
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
+		
+			// Big test with 10,000 numbers
+			Span bigSpan(10000);
+			std::srand(time(NULL));
 			// std::vector<int> nums;
-			// for (int i = 0; i < 10000; ++i)
-			// 	nums.push_back(std::rand());
-	
+			for (int i = 0; i < 10000; ++i)
+				bigSpan.addNumber(std::rand());
+
 			// bigSpan.addRange(nums.begin(), nums.end());
-			// std::cout << "Big Span - Shortest: " << bigSpan.shortestSpan() << std::endl;
-			// std::cout << "Big Span - Longest: " << bigSpan.longestSpan() << std::endl;
+			std::cout << "Big Span - Shortest: " << bigSpan.shortestSpan() << std::endl;
+			std::cout << "Big Span - Longest: " << bigSpan.longestSpan() << std::endl;
 		}
 		catch(const std::exception & e)
 		{
